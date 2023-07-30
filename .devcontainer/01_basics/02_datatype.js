75c20949dc7aa3a7e79d2e06dd59c8fd96a277a3
GauravKhule <138026800+GauravKhule@users.noreply.github.com>
2023-07-30 19:59:29 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3 + 3) // It shows error bcoz we are using nodejs, not browser
+
+let name = "Gaurav"   // String 
+let age = 18  // number
+let isLoggedIn = true // Boolean
+
+// PRIMITIVE
+
+// number  => 2 to power 53
+// bigInt for big number
+// string => "" , '' , ``
+// boolean => true or false
+// null => standalone value, it represent of empty value
+// undefined => value not assign
+// symbol => to represent unique
+
+// NoN - PRIMITIVE
+
+//object
+
+console.log(typeof "Gaurav");  // string
+console.log(typeof age);    // number
+console.log(typeof null);  // object 
+console.log(typeof undefined);  //undefined
