c04b27efc6191f4419790a69c0f600fc16af0a22
GauravKhule <138026800+GauravKhule@users.noreply.github.com>
2023-07-30 18:24:53 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 14352
+let acoountEmail = "gaurav@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+// accountId = 2
+
+console.log(accountId);
+
+accountEmail = "sharma@gmail.com"
+accountPassword = "090909"
+accountCity = "Mumbai"
+
+console.table([accountId,acoountEmail,accountPassword,accountCity,accountState])     /*its use for multi console*/
+
